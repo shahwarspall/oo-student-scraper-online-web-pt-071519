d065200f4f871a0bcc89a5ce7bd4e0338fa2fbb2
Shahwar Spall <shevy.spall@gmail.com>
2019-09-05 04:02:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,9 @@ class Scraper
        name = info.css("h4.student-name").text
        location = info.css("p.student-location").text
        link = info.css("a @href").text
+       {:name => name, :location => location, :profile_url => link}
+    end
+  end
     
   end
 
