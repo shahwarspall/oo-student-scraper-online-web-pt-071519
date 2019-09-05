0dfc29f33a6b0951286ef8ca10b7720113104b32
Shahwar Spall <shevy.spall@gmail.com>
2019-09-05 03:58:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url)).css(".student-card")
     
+    
   end
 
   def self.scrape_profile_page(profile_url)
