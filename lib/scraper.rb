bb8d8295ce4e00f61fdedd8aca65f525aeb9acce
Shahwar Spall <shevy.spall@gmail.com>
2019-09-05 04:26:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,20 +15,19 @@ class Scraper
     end
   end
 
-  def self.scrape_profile_page(profile_url)
-    collection={}
-    doc= Nokogiri::HTML(open(profile_url))
+def self.scrape_profile_page(profile_url)
+    collection = {}
+    doc = Nokogiri::HTML(open(profile_url))
     links = doc.css("div.social-icon-container a").collect{|l| l["href"]}
-    links.each do |link|
+    links.each do |link|     
       if link.include?("twitter")
         collection[:twitter] = link
-      elsif link.include?(".com")
-        collection[:blog] = link 
-      elsif link.include?("github")
-        collection[:github] = link 
       elsif link.include?("linkedin")
         collection[:linkedin] = link 
-      
+      elsif link.include?("github")
+        collection[:github] = link 
+      elsif link.include?(".com")
+        collection[:blog] = link 
         end
       end
     
