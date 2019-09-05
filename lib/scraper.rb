4b87d0aaaab0b47fb5705c616ca8298221f87a5a
Shahwar Spall <shevy.spall@gmail.com>
2019-09-05 04:27:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,10 +22,10 @@ def self.scrape_profile_page(profile_url)
     links.each do |link|     
       if link.include?("twitter")
         collection[:twitter] = link
-      elsif link.include?("linkedin")
-        collection[:linkedin] = link 
       elsif link.include?("github")
         collection[:github] = link 
+      elsif link.include?("linkedin")
+        collection[:linkedin] = link 
       elsif link.include?(".com")
         collection[:blog] = link 
         end
