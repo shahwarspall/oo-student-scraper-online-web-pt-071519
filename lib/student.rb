6a5b29565ce4ecfb1502748ee8a0be4cf77f6810
Shahwar Spall <shevy.spall@gmail.com>
2019-09-05 04:45:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+     attributes_hash.each {|attr, value| self.send("#{attr}=", value)}
+    self
+  end
   end
 
   def self.all
